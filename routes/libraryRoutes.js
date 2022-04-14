4c020769fa8bcb67dad478955804c869a87ee03e
Wren Purcell <purcell856@gmail.com>
2022-04-14 18:06:43 -0400

    deleted create, update and delete routes

--- routes/libraryRoutes.js
+++ routes/libraryRoutes.js
@@ -13,37 +13,6 @@ app.get("/books", async (request, response) => {
   }
 });
 
-//CREATE
-app.post("/books", async(request, response)=>{
-  const book = new bookModel(request.body)
-  try{
-    await book.save()
-    response.send(book)
-  } catch (error){
-    response.status(500).send(error)
-  }
-})
 
-//UPDATE
-app.patch("/books/:id", async (request, response)=>{
-  try{
-    await bookModel.findByIdAndUpdate(request.params.id, request.body)
-    await bookModel.save()
-    response.send(book)
-  } catch(error){
-    response.status(500).send(error)
-  }
-})
-
-//DELETE
-app.delete("/books/:id", async (request, response)=>{
-  try{
-    const books = await bookModel.findByIdAndDelete(request.params.id)
-    if(!books) response.status(404).send("No item found")
-    response.status(200).send()
-  } catch(error){
-    response.status(500).send(error)
-  }
-})
 
 module.exports = app;
